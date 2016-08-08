6e073668750961b77dbc356c7a5ca439f94ab38e
Gilles Crettenand <gilles.crettenand@liip.ch>
2016-08-08 09:06:54 +0200

    wip

--- Markua/blocks.py
+++ Markua/blocks.py
@@ -3,6 +3,41 @@ from CommonMark import blocks
 from Markua.inlines import InlineParser
 
 
+class BlurbBlock(blocks.Block):
+    @staticmethod
+    def blurb_continue(start_char, parser=None, container=None):
+        ln = parser.current_line
+
+        if not parser.indented and \
+               blocks.peek(ln, parser.next_nonspace) == start_char and \
+               blocks.peek(ln, parser.next_nonspace + 1) == '>':
+            parser.advance_next_nonspace()
+            parser.advance_offset(2, False)
+            if blocks.is_space_or_tab(blocks.peek(ln, parser.offset)):
+                parser.advance_offset(1, True)
+        else:
+            return 1
+        return 0
+
+    @staticmethod
+    def continue_(parser=None, container=None):
+        BlurbBlock.blurb_continue('B', parser, container)
+
+    @staticmethod
+    def finalize(parser=None, block=None):
+        return
+
+    @staticmethod
+    def can_contain(t):
+        return True
+
+
+class AsideBlock(BlurbBlock):
+    @staticmethod
+    def continue_(parser=None, container=None):
+        BlurbBlock.blurb_continue('A', parser, container)
+
+
 class BlockStarts(blocks.BlockStarts):
     # We create a new methods array for the following reasons :
     # * Markua disallows HTML blocks
